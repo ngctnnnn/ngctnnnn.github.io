6a77bae56ba4fa7d8b906b9cabefb8345627be0c
ngctnnnn <ngctnnnn@gmail.com>
2021-09-20 14:33:30 +0700

    init

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+yarn install && npm install 
+yarn run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:ngctnnnn/ngctnnnn.github.io.git master:gh-pages
+cd -
