9740fe595f087b2a0d8396bf5f64cf4e70fae48d
Huy <touch@nguyenrk.com>
2021-09-21 21:14:38 +0700

    fix: deploy script

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
-yarn build
+NODE_ENV=production yarn build
 
 cd .vitepress/dist
 
+git init
 git add .
 git commit -m 'deploy'
 git push -f git@github.com:ngctnnnn/blog.git main:gh-pages
